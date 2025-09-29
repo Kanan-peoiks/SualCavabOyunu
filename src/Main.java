80a9baed1db6a9d89cc15b58e9d33fd2c8b0e5e6
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-09-29 19:02:53 +0400

    dowhile/answeredCorrected

--- src/Main.java
+++ src/Main.java
@@ -17,9 +17,11 @@ public class Main {
         int sualIndeksi = 0;
         int duzgunsay=0;
         int sehvsay = 0;
+        boolean answeredCorrected;
 
         while (sualIndeksi < suallar.length){
 //1
+            do{
             System.out.println("1. Sual: " + suallar[0][0]);
             for (int j = 2; j < suallar[0].length; j++) {
                 System.out.println(suallar[0][j]);
@@ -30,6 +32,7 @@ public class Main {
             if (cavab.equals(dogru)) {
                 System.out.println("Doğru!");
                 sualIndeksi+=1;
+                answeredCorrected; // deyer ver cavab= true vvhile sehvdirse davam et, truedursa dayansin
                 duzgunsay+=1;
             } else {
                 System.out.println("Səhv!");
@@ -53,7 +56,7 @@ public class Main {
                 sehvsay+=1;
             }
             cavab="";
-
+//3
             System.out.println("3. Sual: " + suallar[2][0]);
             for (int j = 2; j < suallar[2].length; j++) {
             System.out.println(suallar[2][j]);
@@ -70,8 +73,7 @@ public class Main {
                 sehvsay+=1;
             }
             cavab="";
-
-
+//4
             System.out.println("4. Sual: " + suallar[3][0]);
             for (int j = 2; j < suallar[3].length; j++) {
             System.out.println(suallar[3][j]);
@@ -88,7 +90,7 @@ public class Main {
                 sehvsay+=1;
             }
             cavab="";
-
+//5
             System.out.println("5. Sual: " + suallar[4][0]);
             for (int j = 2; j < suallar[4].length; j++) {
             System.out.println(suallar[4][j]);
@@ -104,8 +106,10 @@ public class Main {
                 System.out.println("Səhv!");
                 sehvsay+=1;
             }
-            cavab="";
+            cavab="";}
+            while (!answeredCorrected);
         }
+        System.out.println("Təbrik edirik! Bütün sualları tamamladınız.");
         System.out.println("Duzgun: " + duzgunsay+ " Sehv: "+ sehvsay);
 
 
